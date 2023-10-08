c8a3c99622929093ff4f23119741a54ce4cbe581
TimelordUK <sjames8888@gmail.com>
2023-10-08 21:28:26 +0100

    move to standard linter

--- src/app/fix-factory.ts
+++ src/app/fix-factory.ts
@@ -2,61 +2,60 @@ import {
   MDEntryType,
   SubscriptionRequestType,
   IMarketDataRequest,
-  IMarketDataSnapshotFullRefresh,
+  IMarketDataSnapshotFullRefresh
 } from '../types'
 import { ILooseObject } from 'jspurefix/dist/collections/collection'
 
 export class FixFactory {
-
   public FullSnapshot (symbol: string, reqId: string, price: number): ILooseObject {
-    // @ts-ignore
+    // @ts-expect-error ts2307
     const snapshot: IMarketDataSnapshotFullRefresh = {
       MDReqID: reqId,
       Symbol: symbol,
 
-        NoMDEntries: [
-          {
-            MDEntryType: MDEntryType.Bid,
-            MDEntryPx: price,
-            MDEntrySize: 1,
-          },
-          {
-            MDEntryType: MDEntryType.Offer,
-            MDEntryPx: price,
-            MDEntrySize: 1,
-          },
-          {
-            MDEntryType: MDEntryType.Imbalance,
-            MDEntryPx: price,
-            MDEntrySize: 1,
-          }
-        ]
+      NoMDEntries: [
+        {
+          MDEntryType: MDEntryType.Bid,
+          MDEntryPx: price,
+          MDEntrySize: 1
+        },
+        {
+          MDEntryType: MDEntryType.Offer,
+          MDEntryPx: price,
+          MDEntrySize: 1
+        },
+        {
+          MDEntryType: MDEntryType.Imbalance,
+          MDEntryPx: price,
+          MDEntrySize: 1
+        }
+      ]
     }
     return snapshot
   }
 
   public BidOfferRequest (symbol: string): ILooseObject {
-    // @ts-ignore
+    // @ts-expect-error ts2307
     const bor: IMarketDataRequest = {
       MDReqID: `#${symbol}#0#`,
       SubscriptionRequestType: SubscriptionRequestType.SnapshotPlusUpdates,
       MarketDepth: 0,
-        NoMDEntryTypes: [
-          {
-            MDEntryType: MDEntryType.Bid
-          },
-          {
-            MDEntryType: MDEntryType.Offer
-          },
-          {
-            MDEntryType: MDEntryType.Imbalance
-          }
-        ],
-        NoRelatedSym: [
-          {
-            Symbol: symbol
-          }
-        ]
+      NoMDEntryTypes: [
+        {
+          MDEntryType: MDEntryType.Bid
+        },
+        {
+          MDEntryType: MDEntryType.Offer
+        },
+        {
+          MDEntryType: MDEntryType.Imbalance
+        }
+      ],
+      NoRelatedSym: [
+        {
+          Symbol: symbol
+        }
+      ]
     }
     return bor
   }
